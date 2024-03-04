e340eb9e9b21b9b9e0cd123855268b519293c4d5
Hazrat Ali <89822613+hzratali@users.noreply.github.com>
2024-03-04 17:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-//your JS code here. If required.
-function soundPlay(audioPlayer){ 
-	let audioPlayer = document.getElementsByClassName("btn");
-	audioPlayer.src = "D:\New folder\sound\short-crowd-cheer-6713.mp3"+ soundbar;
-	audioPlayer.play();
-}
-btns.addEventListener("click", soundPlay);
-function soundPlay(audioPlayer){ 
-    let audioPlayer = document.getElementsByClassName("btn");
-    audioPlayer.src = "D:\New folder\sound\boo-6377.mp3" + soundbar;
-    audioPlayer.play();
-}
-btns.addEventListener("click", soundPlay);
-function soundStop(){
-    let audioPlayer = getElementsByClassName("stop");
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+function stop() {
+  var audio = document.getElementById("audio");
+  audio.pause();
+  audio.currentTime = 0;
 }
